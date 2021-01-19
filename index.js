3e434dff1103fae4ab9a8a2d214bed135d9667cd
Donavan Carvalho <donavancarvalho@terra.com.br>
2021-01-19 01:09:35 -0300

    Integration with amazon s3

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ require('dotenv/config');
 
 const multer = require('multer');
 const express = require('express');
+const AWS = require('aws-sdk');
+const { v4: uuid } = require('uuid');
 
 const app = express();
+
+const s3 = new AWS.S3({
+    accessKeyId: process.env.AWS_ID,
+    secretAccessKey: process.env.AWS_SECRET
+});
 
 const storage = multer.memoryStorage({
     destination: function(req, file, callback){
@@ -14,11 +21,28 @@ const storage = multer.memoryStorage({
 const upload = multer({storage}).single('image');
 
 app.post('/upload', upload, (req, res) => {
-    console.log(req.file);
+    const myFile = req.file.originalname.split('');
+    const fileType = myFile[myFile.length - 1];
 
-    res.send({
-        message: 'Hello World'
+    const params = {
+        Bucket: process.env.AWS_BUCKET_NAME,
+        Key: `${uuid()}.${fileType}`,
+        Body: req.file.buffer
+    }
+
+    s3.upload(params, (error, data) => {
+        if(error){
+            return res.status(500).send(error);
+        }
+
+        res.status(200).send(data);
     });
+
+    // console.log(req.file);
+
+    // res.send({
+    //     message: 'Hello World'
+    // });
 });
 
 const server = app.listen(process.env.PORT || 3000, () => {
