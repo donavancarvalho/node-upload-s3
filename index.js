06af961f0d7943581c2e6330da7a789fabd32639
Donavan Carvalho <donavancarvalho@terra.com.br>
2021-01-18 23:59:49 -0300

    the upload route is working

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
+require('dotenv/config');
+
+const multer = require('multer');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
+const storage = multer.memoryStorage({
+    destination: function(req, file, callback){
+        callback(null, '');
+    }
+});
+
+const upload = multer({storage}).single('image');
+
+app.post('/upload', upload, (req, res) => {
+    console.log(req.file);
+
     res.send({
         message: 'Hello World'
     });
